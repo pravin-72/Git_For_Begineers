de117c0915c56f262c8a010266a0d6a44190fe0d
Pravin <pravinmurugesankkp@gmail.com>
2026-08-30 15:32:21 +0530

    new modifed subtract code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums, subract_nums
 
 
 if __name__ == "__main__":
     result = add_nums(5, 10)
     print(f"The sum is: {result}")
+    print(f"The difference is: {subract_nums(10, 5)}")
--- utils/calculator.py
+++ utils/calculator.py
@@ -1,2 +1,5 @@
 def add_nums(num1, num2):
     return num1 + num2
+
+def subract_nums(num1, num2):
+    return num1 - num2
