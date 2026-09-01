7bb0d79de822e1b8e57005ac6122bfb8ecabd23e
Pravin <pravinmurugesankkp@gmail.com>
2026-09-01 22:29:26 +0530

    formatted calculato.py

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,12 +1,12 @@
 def add_nums(num1, num2):
-    return num1 + num2
+    return num1+num2
 
 def divide_nums(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero.")
     return a / b
 def subract_nums(num1, num2):
-    return num1 - num2
+    return num1-num2
 
 def power_nums(base, exponent):
     return base ** exponent
